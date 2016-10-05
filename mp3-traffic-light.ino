1bd1d1c63ab49366c7f9a8424e7aadff4016313d
Richard Regeer <r.regeer@coolblue.nl>
2016-10-05 09:48:59 +0200

    add mp3 and ethernet code

--- mp3-traffic-light.ino
+++ mp3-traffic-light.ino
@@ -1,118 +1,144 @@
 #include <SPI.h>
-
 #include <Adafruit_NeoPixel.h>
-
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
-#define PIN            6
-
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      9
-
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-int delayval = 500; // delay for half a second
-
-//Add the SdFat Libraries
 #include <SdFat.h>
 #include <SdFatUtil.h>
-
-//and the MP3 Shield Library
 #include <SFEMP3Shield.h>
-
 #include <Ethernet.h>
 
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char server[] = "www.google.com";    // name address for Google (using DNS)
+//////////// Serial /////////////////////////////
+#define BAUD_RATE 9600
 
-// Set the static IP address to use if the DHCP fails to assign
-IPAddress ip(192, 168, 0, 177);
+//////////// Ethernet /////////////////////////////
+const byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+// Set the static IP address to use if the DHCP fails to assign 
+const IPAddress ip(192, 168, 1, 10);
+EthernetClient networkClient;
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
-EthernetClient client;
+//////////// Traffic light service ////////////////
+#define TRAFFIC_LIGHT_SERVICE_PORT 3000
+const char trafficLightHost[] = "192.168.1.5";
+unsigned int lastConnectionTime = 0;
+const unsigned int pollInterval = 1500;
 
+//////////// Timing ///////////////////////////////
+#define DELAY 500
+
+//////////// RGB Pixels ///////////////////////////
+#define RGB_PIXELS 9
+#define PIN_RGB_LED 13
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(RGB_PIXELS, PIN_RGB_LED, NEO_GRB + NEO_KHZ800);
+
+//////////////// MP3 /////////////////////////////
 SdFat sd;
 SFEMP3Shield MP3player;
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup() {
+  // Initialize serial modem
+  setupSerialModem();
+  setupRGBLeds();
+//  setupEthernet();
+  setupMp3Player();
+}
 
-  uint8_t result; //result code from some function as to be tested at later time.
-
-  Serial.begin(9600);
+void setupSerialModem() {
+  Serial.begin(BAUD_RATE);  
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  Serial.print(F("F_CPU = "));
-  Serial.println(F_CPU);
-  Serial.print(F("Free RAM = ")); // available in Version 1.0 F() bases the string to into Flash, to use less SRAM.
-  Serial.print(FreeRam(), DEC);  // FreeRam() is provided by SdFatUtil.h
-  Serial.println(F(" Should be a base line of 1028, on ATmega328 when using INTx"));
+  Serial.println(F("Starting."));
+}
 
-  pixels.begin(); // This initializes the NeoPixel library.
-    
-  Serial.println("Start Ethernet connection...");
-  // start the Ethernet connection:
+void setupRGBLeds() {
+  pixels.begin(); 
+  resetLights();
+  Serial.println(F("Lights ready."));
+}
+
+void setupEthernet() {
   if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
+    Serial.println(F("Failed to configure Ethernet using DHCP."));
     // try to congifure using IP address instead of DHCP:
     Ethernet.begin(mac, ip);
   }
-  // give the Ethernet shield a second to initialize:
-  delay(1000);
-  Serial.println("connecting...");
+  
+  Serial.println(F("Ethernet ready."));
+}
 
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
-    Serial.println("connected");
-    // Make a HTTP request:
-    client.println("GET /search?q=arduino HTTP/1.1");
-    client.println("Host: www.google.com");
-    client.println("Connection: close");
-    client.println();
-  } else {
-    // if you didn't get a connection to the server:
-    Serial.println("connection failed");
+void setupMp3Player() {
+  // Initialize SD card.
+  if(!sd.begin(SD_SEL, SPI_FULL_SPEED))  {
+    sd.initErrorHalt();
+  }
+  if(!sd.chdir("/")) {
+    sd.errorHalt("sd.chdir");
   }
 
-  //Initialize the SdCard.
-  if(!sd.begin(SD_SEL, SPI_FULL_SPEED)) sd.initErrorHalt();
-  // depending upon your SdCard environment, SPI_HAVE_SPEED may work better.
-  if(!sd.chdir("/")) sd.errorHalt("sd.chdir");
-
-  //Initialize the MP3 Player Shield
-  result = MP3player.begin();
-  //check result, see readme for error codes.
-  if(result != 0) {
+  // Initialize the MP3 Player Shield.
+  uint8_t mp3PlayerResult;
+  mp3PlayerResult = MP3player.begin();
+  
+  if(mp3PlayerResult != 0 && mp3PlayerResult !=6 ) {
     Serial.print(F("Error code: "));
-    Serial.print(result);
-    Serial.println(F(" when trying to start MP3 player"));
-    if( result == 6 ) {
-      Serial.println(F("Warning: patch file not found, skipping.")); // can be removed for space, if needed.
-      Serial.println(F("Use the \"d\" command to verify SdCard can be read")); // can be removed for space, if needed.
-    }
+    Serial.println(mp3PlayerResult);
   }
+  
+  Serial.println(F("MP3 player ready."));  
 }
 
 void loop() {
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+//  // If interval is past get the last status from the traffic light service.
+//  if (millis() - lastConnectionTime > pollInterval) {
+//    getTrafficLightStatus();
+//  }
+//
+//  if (networkClient.available()) {
+//    char c = networkClient.read();
+//    Serial.write(c);
+//    resetLights();
+//    setLight(1, 1, 150, 0); 
+//  }
+}
 
-  for(int i=0;i<NUMPIXELS;i++){
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+void resetLights() {
+  for(byte i=0;i<RGB_PIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(0,0,0));
+    pixels.show();
+  }
+}
 
-    pixels.show(); // This sends the updated pixel color to the hardware.
+void setLight(byte lightNr, byte R, byte G, byte B) {
+  pixels.setPixelColor(lightNr, pixels.Color(R,G,B));
+  pixels.show();
+}
 
-    delay(delayval); // Delay for a period of time (in milliseconds).
+void playTrack(char trackName[]) {
+  uint8_t result;
+
+  result = MP3player.playMP3(trackName, 0);
+ 
+  if(result != 0) {
+    Serial.print(F("Error code: "));
+    Serial.print(result);
+    Serial.println(F(" when trying to play track"));
+  }
+}
+
+void getTrafficLightStatus() {
+  networkClient.stop();
+
+  if (networkClient.connect(trafficLightHost, TRAFFIC_LIGHT_SERVICE_PORT)) {
+    // send the HTTP GET request:
+    networkClient.println(F("GET /status/traffic-light HTTP/1.1"));
+    //networkClient.println(F("Host: www.arduino.cc"));
+    networkClient.println(F("User-Agent: traffic-light"));
+    networkClient.println(F("Connection: close"));
+    networkClient.println();
+
+    // Note the time that the connection was made:
+    lastConnectionTime = millis();
+  } else {
+    Serial.println(F("Get status failed."));
   }
 }
